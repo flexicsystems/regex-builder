efabc5172e189122d0cd0d7b59ee35ee720efe89
Hendrik Legge <admin@themepoint.de>
2022-11-06 17:58:06 +0100

    Fixed

--- src/Expr/Digit.php
+++ src/Expr/Digit.php
@@ -18,6 +18,6 @@ final class Digit extends ExpressionInterface
 {
     public function __construct(int|float $expression)
     {
-        parent::__construct((int) $expression);
+        parent::__construct((string) $expression);
     }
 }
